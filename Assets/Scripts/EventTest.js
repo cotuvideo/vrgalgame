d15739640f650c57254eb05c2998b1136eb9a901
cotuvideo <cotuvideo@gmail.com>
2017-02-11 18:04:09 +0900

    イベントテスト用に球体追加

---
+++ Assets/Scripts/EventTest.js
@@ -0,0 +1,19 @@
+#pragma strict
+
+function Start () {
+	
+}
+
+function Update () {
+	
+}
+
+function OnPointerEnter()
+{
+	Debug.Log("enter");
+}
+
+function OnPointerExit()
+{
+	Debug.Log("exit");
+}
