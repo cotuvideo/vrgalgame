65de9cb5232c02b6389ab7cdce3cd42820b88512
cotuvideo <cotuvideo@gmail.com>
2017-02-24 01:50:01 +0900

    テキスト表示

--- Assets/Scripts/EventTest.js
+++ Assets/Scripts/EventTest.js
@@ -1,5 +1,6 @@
 #pragma strict
 
+public var text: TextMesh;
 public var sound00: AudioClip;
 public var sound01: AudioClip;
 public var cnt : int = 0;
@@ -15,13 +16,15 @@ function Update () {
 function OnPointerEnter()
 {
 	Debug.Log("enter");
-	Global.anim.SetBool ("Next", true);
+	Global.anim.SetBool("Next", true);
 	if(++cnt%2 == 1)
 	{
+		text.text = "ｘｘｘするぅ？";
 		GetComponent.<AudioSource>().PlayOneShot(sound00);
 	}
 	else
 	{
+		text.text = "のんのん";
 		GetComponent.<AudioSource>().PlayOneShot(sound01);
 	}
 }
