55daa41c725a2d6268b779792f574fa8ce432f59
cotuvideo <cotuvideo@gmail.com>
2017-02-23 21:49:10 +0900

    アニメチェンジのタイミングでボイス再生

--- Assets/Scripts/EventTest.js
+++ Assets/Scripts/EventTest.js
@@ -1,5 +1,9 @@
 #pragma strict
 
+public var sound00: AudioClip;
+public var sound01: AudioClip;
+public var cnt : int = 0;
+
 function Start () {
 	
 }
@@ -12,6 +16,14 @@ function OnPointerEnter()
 {
 	Debug.Log("enter");
 	Global.anim.SetBool ("Next", true);
+	if(++cnt%2 == 1)
+	{
+		GetComponent.<AudioSource>().PlayOneShot(sound00);
+	}
+	else
+	{
+		GetComponent.<AudioSource>().PlayOneShot(sound01);
+	}
 }
 
 function OnPointerExit()
